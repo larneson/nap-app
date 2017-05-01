590e79e95fd95f6b2cd8dbe46a8a50d09b834704
Link Arneson <linkarneson@gmail.com>
2017-04-30 18:24:29 -0700

    get location stuff

--- nap-app/ViewController.swift
+++ nap-app/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    var currentBoundary = MKCircle()
+    
+    let regionRadius: CLLocationDistance = 1000
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manager.delegate = self 
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        if CLLocationManager.authorizationStatus() != CLAuthorizationStatus.authorizedWhenInUse
+        {
+            manager.requestWhenInUseAuthorization()
+            if let currentLocation = manager.location {
+                centerMapOnLocation(location: currentLocation)
+            }
+        }
+        
+
+        manager.startUpdatingLocation()
+        if let currentLocation = manager.location {
+            centerMapOnLocation(location: currentLocation)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +45,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("lolfail", error)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // most recent location update at the end of the array
+        //let latestLocation = locations[locations.count - 1]
+        // do something
+        //print("this will never work :(")
+    }
 
+    @IBAction func selectButtonPressed(_ sender: Any) {
+        print(mapView.centerCoordinate)
+        mapView.remove(currentBoundary)
+        currentBoundary = MKCircle(center: mapView.centerCoordinate, radius: regionRadius / 2)
+        mapView.add(currentBoundary)
+    }
 }
 
